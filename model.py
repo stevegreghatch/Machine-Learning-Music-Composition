856bfcdd413fbe7a5f0715a0ef210b2885b6bc61
Steven Hatch <90797071+stevegreghatch@users.noreply.github.com>
2021-11-08 15:17:42 -0800

    Update model.py

--- model.py
+++ model.py
@@ -115,7 +115,6 @@ def LSTMFunction():
     print(inputShape)
 
     model = Sequential()
-    # model version 1
     model.add(CuDNNLSTM(
         256,
         input_shape=inputShape,
@@ -127,18 +126,6 @@ def LSTMFunction():
     model.add(Dense(numberOfUniqueElements))
     model.add(Activation('softmax'))
     model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
-
-    # model version 2
-    '''
-    model.add(Bidirectional(CuDNNLSTM(
-        128,
-        input_shape=inputShape,
-        return_sequences=True
-    )))
-    model.add(Bidirectional(CuDNNLSTM(128, return_sequences=True)))
-    model.add(TimeDistributed(Dense(64, activation='sigmoid')))
-    model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
-    '''
 
     print(model.summary())
 
@@ -193,7 +180,6 @@ def LSTMFunction():
     # print(inputShape)
 
     model = Sequential()
-    # model version 1
     model.add(CuDNNLSTM(
         256,
         input_shape=inputShape,
@@ -206,155 +192,149 @@ def LSTMFunction():
     model.add(Activation('softmax'))
     model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 
-    # model version 2
-    '''
-    model.add(Bidirectional(CuDNNLSTM(
-        128,
-        input_shape=inputShape,
-        return_sequences=True
-    )))
-    model.add(Bidirectional(CuDNNLSTM(128, return_sequences=True)))
-    model.add(TimeDistributed(Dense(64, activation='sigmoid')))
-    model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
-    '''
-
     model.load_weights('model-best-.hdf5')
 
     print(model.summary())
 
-    # get prediction from model
-    # start by getting sequence from model input
-    startInt = numpy.random.randint(0, len(modelInputForOutput)-1)
-    # print('startInt')
-    # print(startInt)
-    sequence = modelInputForOutput[startInt]
-    # print('sequence')
-    # print(sequence)
-    # print('len(sequenceFromStartInt)')
-    # print(len(sequenceFromStartInt))
-
-    predictedSong = []
-    # set number of note:duration elements for output composition
-    for i in range(500):
-        # reshape sequence to 3D
-        predictionInputReshaped = numpy.reshape(sequence, (1, len(sequence), 1))
-        # print('predictionInputReshaped')
-        # print(predictionInputReshaped)
-
-        # normalize sequence
-        predictionInputNormalized = predictionInputReshaped / float(numberOfUniqueElements)
-        # print('predictionInputNormalized')
-        # print(predictionInputNormalized)
-
-        # get prediction
-        prediction = model.predict(predictionInputNormalized, verbose=0)
-        # print('PREDICTION ---- PREDICTION OUTPUT')
-        # print(prediction)
-
-        # denormalize prediction
-        denormalizedPrediction = prediction * float(numberOfUniqueElements)
-        # print('denormalizedPrediction')
-        # print(denormalizedPrediction)
-
-        # round to nearest int
-        denormalizedPredictionRounded = (numpy.round(denormalizedPrediction, 0)).astype(int)
-        # print('denormalizedPredictionRounded')
-        # print(denormalizedPredictionRounded)
-
-        # reshape
-        denormalizedPredictionRoundedReshaped = numpy.reshape(denormalizedPredictionRounded, -1)
-        # print('denormalizedPredictionRoundedReshaped')
-        # print(denormalizedPredictionRoundedReshaped)
-        # print('len(denormalizedPredictionRoundedReshaped)')
-        # print(len(denormalizedPredictionRoundedReshaped))
-        # print('numberOfUniqueElements')
-        # print(numberOfUniqueElements)
-
-        # get index with highest frequency and its location from array
-        # this target output array represents numberOfUniqueElements
-        highestInt = 0
-        for element in denormalizedPredictionRoundedReshaped:
-            if element > highestInt:
-                highestInt = element
-        # index = numpy.where(denormalizedPredictionRoundedReshaped == highestInt)[0]
-        # print('highestIntAndIndex')
-        # print(highestInt, index)
-        indexWithMostPredictions = int(numpy.argmax(prediction))
-        # print('indexWithMostPredictions')
-        # print(indexWithMostPredictions)
-
-        # convert int back to note and duration and append to list
-        notePitchAndDurationMatch = noteAndDurationToIntWithEnumerate[indexWithMostPredictions]
-        notePitchAndDurationMatch = notePitchAndDurationMatch[1]
-        # print('notePitchAndDurationMatch')
-        # print(notePitchAndDurationMatch)
-
-        # append to predicted song list for later midi conversion
-        predictedSong.append(notePitchAndDurationMatch)
-        # print('predictedSong')
-        # print(predictedSong)
-
-        sequence.append(indexWithMostPredictions)
-        # print('sequenceWithNewIndexAppended')
+    # get predictions from model
+    # get desired number output compositions
+    for f in range(10):
+        # start by getting sequence from model input
+        startInt = numpy.random.randint(0, len(modelInputForOutput)-1)
+        # print('startInt')
+        # print(startInt)
+        sequence = modelInputForOutput[startInt]
+        # print('sequence')
         # print(sequence)
+        # print('len(sequenceFromStartInt)')
+        # print(len(sequenceFromStartInt))
 
-        # remove first element from sequence to create new sequence for next prediction
-        sequence = sequence[1:len(sequence)]
-        # print('sequenceWithFirstElementRemoved')
-        # print(sequence)
-        # print('\n')
-    print('predictedSong')
-    print(predictedSong)
+        predictedSong = []
+        # set number of note:duration elements for output composition
+        for i in range(500):
+            # reshape sequence to 3D
+            predictionInputReshaped = numpy.reshape(sequence, (1, len(sequence), 1))
+            # print('predictionInputReshaped')
+            # print(predictionInputReshaped)
 
-    # create output composition
-    outputComposition = []
-    for element in predictedSong:
-        noteToSet = element.split(':')[0]
-        durationToSet = element.split(':')[1]
-        # convert duration to float if in fraction format ex. '17/3'
-        if '/' in durationToSet:
-            durationToSet = durationToSet.split('/')
-            num = int(durationToSet[0])
-            denom = int(durationToSet[1])
-            durationToSet = "{:.2f}".format(float(num/denom))
-        # regular note
-        if len(noteToSet) <= 3:
-            newNote = note.Note(noteToSet)
-            newNote.quarterLength = float(durationToSet)
-            newNote.storedInstrument = instrument.Piano()
-            outputComposition.append(newNote)
-        # rest
-        elif 'rest' in element:
-            newRest = note.Rest(element)
-            newRest.quarterLength = float(durationToSet)
-            newRest.storedInstrument = instrument.Piano()
-            outputComposition.append(newRest)
-        # chord
-        else:
-            # print('noteToSet')
-            # print(noteToSet)
-            cToFiler = ["[", "'", "]", ","]
-            for c in cToFiler:
-                noteToSet = noteToSet.replace(c, '')
-            # print('noteToSetUpdated')
-            # print(noteToSet)
-            noteToSetSplit = noteToSet.split()
-            # print('noteToSetSplit')
-            # print(noteToSetSplit)
-            newChord = chord.Chord(noteToSetSplit)
-            # print('newChord')
-            # print(newChord)
-            # print('newChord.pitches')
-            # print(newChord.pitches)
-            newChord.storedInstrument = instrument.Piano()
-            newChord.quarterLength = float(durationToSet)
-            outputComposition.append(newChord)
+            # normalize sequence
+            predictionInputNormalized = predictionInputReshaped / float(numberOfUniqueElements)
+            # print('predictionInputNormalized')
+            # print(predictionInputNormalized)
+
+            # get prediction
+            prediction = model.predict(predictionInputNormalized, verbose=0)
+            # print('PREDICTION ---- PREDICTION OUTPUT')
+            # print(prediction)
+
+            # denormalize prediction
+            denormalizedPrediction = prediction * float(numberOfUniqueElements)
+            # print('denormalizedPrediction')
+            # print(denormalizedPrediction)
+
+            # round to nearest int
+            denormalizedPredictionRounded = (numpy.round(denormalizedPrediction, 0)).astype(int)
+            # print('denormalizedPredictionRounded')
+            # print(denormalizedPredictionRounded)
+
+            # reshape
+            denormalizedPredictionRoundedReshaped = numpy.reshape(denormalizedPredictionRounded, -1)
+            # print('denormalizedPredictionRoundedReshaped')
+            # print(denormalizedPredictionRoundedReshaped)
+            # print('len(denormalizedPredictionRoundedReshaped)')
+            # print(len(denormalizedPredictionRoundedReshaped))
+            # print('numberOfUniqueElements')
+            # print(numberOfUniqueElements)
+
+            # get index with highest frequency and its location from array
+            # this target output array represents numberOfUniqueElements
+            highestInt = 0
+            for element in denormalizedPredictionRoundedReshaped:
+                if element > highestInt:
+                    highestInt = element
+            # index = numpy.where(denormalizedPredictionRoundedReshaped == highestInt)[0]
+            # print('highestIntAndIndex')
+            # print(highestInt, index)
+            indexWithMostPredictions = int(numpy.argmax(prediction))
+            # print('indexWithMostPredictions')
+            # print(indexWithMostPredictions)
+
+            # convert int back to note and duration and append to list
+            notePitchAndDurationMatch = noteAndDurationToIntWithEnumerate[indexWithMostPredictions]
+            notePitchAndDurationMatch = notePitchAndDurationMatch[1]
+            # print('notePitchAndDurationMatch')
+            # print(notePitchAndDurationMatch)
+
+            # append to predicted song list for later midi conversion
+            predictedSong.append(notePitchAndDurationMatch)
+            # print('predictedSong')
+            # print(predictedSong)
+
+            sequence.append(indexWithMostPredictions)
+            # print('sequenceWithNewIndexAppended')
+            # print(sequence)
+
+            # remove first element from sequence to create new sequence for next prediction
+            sequence = sequence[1:len(sequence)]
+            # print('sequenceWithFirstElementRemoved')
+            # print(sequence)
+            # print('\n')
+        print('predictedSong')
+        print(predictedSong)
+
+        # create output composition
+        outputComposition = []
+        for element in predictedSong:
+            noteToSet = element.split(':')[0]
+            durationToSet = element.split(':')[1]
+            # convert duration to float if in fraction format ex. '17/3'
+            if '/' in durationToSet:
+                durationToSet = durationToSet.split('/')
+                num = int(durationToSet[0])
+                denom = int(durationToSet[1])
+                durationToSet = "{:.2f}".format(float(num/denom))
+            # regular note
+            if len(noteToSet) <= 3:
+                newNote = note.Note(noteToSet)
+                newNote.quarterLength = float(durationToSet)
+                newNote.storedInstrument = instrument.Piano()
+                outputComposition.append(newNote)
+            # rest
+            elif 'rest' in element:
+                newRest = note.Rest(element)
+                newRest.quarterLength = float(durationToSet)
+                newRest.storedInstrument = instrument.Piano()
+                outputComposition.append(newRest)
+            # chord
+            else:
+                # print('noteToSet')
+                # print(noteToSet)
+                cToFiler = ["[", "'", "]", ","]
+                for c in cToFiler:
+                    noteToSet = noteToSet.replace(c, '')
+                # print('noteToSetUpdated')
+                # print(noteToSet)
+                noteToSetSplit = noteToSet.split()
+                # print('noteToSetSplit')
+                # print(noteToSetSplit)
+                newChord = chord.Chord(noteToSetSplit)
+                # print('newChord')
+                # print(newChord)
+                # print('newChord.pitches')
+                # print(newChord.pitches)
+                newChord.storedInstrument = instrument.Piano()
+                newChord.quarterLength = float(durationToSet)
+                outputComposition.append(newChord)
+            # print('outputComposition')
+            # print(outputComposition)
+            # print('len(outputComposition)')
+            # print(len(outputComposition))
         # print('outputComposition')
         # print(outputComposition)
-        # print('len(outputComposition)')
-        # print(len(outputComposition))
-    # print('outputComposition')
-    # print(outputComposition)
-    midiStream = stream.Stream(outputComposition)
-    midiStream.write('midi', fp='Midi_Output/outputComposition.mid')
-    midiStream.show('midi')
+        midiStream = stream.Stream(outputComposition)
+
+        n = 0
+        while os.path.exists('-Midi_Output/outputComposition%s.mid' % n):
+            n += 1
+        midiStream.write('midi', fp=('-Midi_Output/outputComposition%s.mid' % n))
+        # midiStream.show('midi')
